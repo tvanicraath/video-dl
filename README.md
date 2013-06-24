f2a9194949b95520643feb94ab166896e67b3740
rachit <iamrachiit@gmail.com>
2013-06-25 00:09:36 +0530

    First working commit -v1.0

---
+++ README.md
@@ -0,0 +1,16 @@
+#INTRODUCTION
+video-dl is a script to download videos from youtube and some other sites.
+
+#USAGE
+There are 2 scripts.
+1) video-dl:	video-dl $url	OR	video-dl $website $id
+2) grab-url:	somehow activate the script keeping your browser focused. (Tray icon or Hotkeys)
+
+#SUPPORTED SITES
+Currently, the supported sites are-
+tubemotion, flashx.tv, facebook videos (in grab-url), tahuko.com (a Gujarati songs website) and all supported sites of your youtube-dl (https://github.com/rg3/youtube-dl/)
+
+#DESCRIPTION
+For the time being, I have just made a rude script, but as and when time permits, I am looking to port them to python similar to youtube-dl.
+No other tools required except python and bash.
+Feel free to add your own patches for the sites you like.
---
+++ video-dl_files/tahuko_files/soundFile_decoder.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+import sys
+
+def int2bin(n):
+    assert n >= 0, "Number must be positive"
+    res = ""
+    if n == 0: return "0"
+    while True:
+        if n == 0:
+            return res
+        elif n % 2 == 0:
+            res = "0" + res
+        else:
+            res = "1" + res
+            n -= 1
+        n /= 2
+
+def decode(source):
+    key = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_-"
+    binary = ""
+
+    for char in source:
+        code = key.find(char) # look up each char in the code key
+        binary += int2bin(code).rjust(6, "0") # append char index as a six bit binary string
+    
+    chunks = [] 
+    # split string into segments of length 8
+    while len(binary) > 0:
+        chunks.append(binary[:8]) 
+        binary = binary[8:]
+
+    chars = [chr(int(x, 2)) for x in chunks] # convert the segments to chars
+    return "".join(chars)
+
+# http://www.mabd.se/?p=682
+#test = "aHR0cDovL3d3dy5tYWJkLnNlL3VwbG9hZC8yMDEwLzEyL0RlbWVudG9yZXIubXAzA"
+#test = "aHR0cDovL3RhaHVrby5jb20vYXVkaW9maWxlc2ZvcnRhaHVrby9LYWFuZHUgTWFyZHl1IC0gTmVoYS5tcDM"
+test = sys.argv[1]
+print(decode(test))
+
